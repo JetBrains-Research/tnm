9b6064b5970569caaa4cf716a3d79c12a21987c7
Vladimir Kovalenko <vladimir.kovalenko@jetbrains.com>
2020-11-17 15:40:29 +0300

    add application plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,18 +2,26 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.10"
+    id("application")
 }
+
+application {
+    mainClassName = "gitMiners.ChangedFilesMinerKt"
+}
+
 group = "me.nikolaisv"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
+
 dependencies {
     implementation("org.eclipse.jgit:org.eclipse.jgit:5.9.0.202009080501-r")
     implementation("com.google.code.gson:gson:2.8.6")
     testImplementation(kotlin("test-junit"))
 }
+
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
