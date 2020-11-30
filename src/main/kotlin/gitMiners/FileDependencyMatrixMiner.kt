98f54d7fe4f6497c9ac57fcf0fefc97d10172e35
NikolaiSv <nikolai.sviridov@jetbrains.com>
2020-11-30 19:36:34 +0300

    Refactor FileDependencyMatrixMiner.kt array -> hashmap

--- src/main/kotlin/gitMiners/FileDependencyMatrixMiner.kt
+++ src/main/kotlin/gitMiners/FileDependencyMatrixMiner.kt
@@ -5,14 +5,15 @@ import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.internal.storage.file.FileRepository
 import org.eclipse.jgit.lib.ObjectReader
 import org.eclipse.jgit.revwalk.RevCommit
-import util.FileMapper
 import java.io.File
 
 /**
- * Class for parsing  file dependency matrix based on https://ieeexplore.ieee.org/abstract/document/5740929
+ * Class for parsing  file dependency matrix
+ * based on https://ieeexplore.ieee.org/abstract/document/5740929
  * For example:
  * Change sets {A,B,C} and {A,B} the dependency matrix entries in D would be
  * D[A,B] = 2, D[A,C] = 1, and D[B,C] = 1
+ *
  */
 class FileDependencyMatrixMiner(override val repository: FileRepository) : GitMiner() {
     override val git = Git(repository)
@@ -20,28 +21,28 @@ class FileDependencyMatrixMiner(override val repository: FileRepository) : GitMi
     override val gson: Gson = Gson()
 
     private val changedFilesMiner = ChangedFilesMiner(repository)
-    private val changedFiles = mutableListOf<List<Int>>()
-    private lateinit var fileDependencyMatrix: Array<Array<Int>>
+    private val fileDependencyMatrix: HashMap<Int, HashMap<Int, Int>> = HashMap()
 
     override fun process(currCommit: RevCommit, prevCommit: RevCommit) {
-        changedFiles.add(getChangedFiles(currCommit, prevCommit, reader, git).toList())
-    }
-
-    override fun run() {
-        super.run()
-
-        val size = FileMapper.lastFileId
-        fileDependencyMatrix = Array(size) { Array(size) { 0 } }
-        for (change in changedFiles) {
-            for ((index, currFile) in change.withIndex()) {
-                for (otherFile in change.subList(index, change.lastIndex)) {
-                    if (currFile == otherFile)
-                        continue
-                    fileDependencyMatrix[currFile][otherFile] += 1
-                    fileDependencyMatrix[otherFile][currFile] += 1
-                }
+        val listOfChangedFiles = getChangedFiles(currCommit, prevCommit, reader, git).toList()
+        for ((index, currFile) in listOfChangedFiles.withIndex()) {
+            for (otherFile in listOfChangedFiles.subList(index, listOfChangedFiles.lastIndex)) {
+                if (currFile == otherFile)
+                    continue
+                increment(currFile, otherFile)
+                increment(otherFile, currFile)
             }
         }
+    }
+
+    private fun increment(fileId1: Int, fileId2: Int) {
+        val newValue = fileDependencyMatrix
+            .computeIfAbsent(fileId1) {HashMap()}
+            .computeIfAbsent(fileId2) {0}
+            .inc()
+
+        fileDependencyMatrix
+            .computeIfAbsent(fileId1) {HashMap()} [fileId2] = newValue
     }
 
     override fun saveToJson() {
