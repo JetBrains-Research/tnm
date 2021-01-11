ea6e07a7993c986a9862014f816348b95227327a
NikolaiSv <nikolai.sviridov@jetbrains.com>
2021-01-12 00:12:10 +0300

    Delete unused dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,6 @@ dependencies {
     implementation("org.eclipse.jgit:org.eclipse.jgit:5.9.0.202009080501-r")
     // TODO: to big mb change
     implementation("org.deeplearning4j:deeplearning4j-core:1.0.0-beta6")
-    implementation("org.nd4j:nd4j-native-platform:1.0.0-beta6")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
     implementation("com.github.ajalt.clikt:clikt:3.1.0")
     testImplementation(kotlin("test-junit"))
