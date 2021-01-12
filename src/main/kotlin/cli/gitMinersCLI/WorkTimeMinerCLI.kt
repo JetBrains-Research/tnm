44954bd0d9639e1b07006eb77fb94fa83193c03b
NikolaiSv <nikolai.sviridov@jetbrains.com>
2021-01-12 13:49:02 +0300

    Add multithreading WorkTimeMinerCLI

--- src/main/kotlin/cli/gitMinersCLI/WorkTimeMinerCLI.kt
+++ src/main/kotlin/cli/gitMinersCLI/WorkTimeMinerCLI.kt
@@ -3,11 +3,11 @@ package cli.gitMinersCLI
 import cli.InfoCLI
 import gitMiners.WorkTimeMiner
 
-class WorkTimeMinerCLI : GitMinerCLI(
+class WorkTimeMinerCLI : MultithreadedGitMinerCLI(
     InfoCLI("WorkTimeMiner", "Mine commits time distribution for each developer")
 ) {
     override fun run() {
-        val miner = WorkTimeMiner(repository!!, branches)
+        val miner = WorkTimeMiner(repository!!, branches, numThreads = numThreads)
         miner.run()
         miner.saveToJson(resources!!)
     }
