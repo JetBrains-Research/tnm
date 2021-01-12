f5caa61bda0920c76ebed86646fb03d7ac795c6f
NikolaiSv <nikolai.sviridov@jetbrains.com>
2021-01-12 13:20:10 +0300

    Add MultithreadedGitMinerCLI

--- src/main/kotlin/cli/gitMinersCLI/FileDependencyMatrixMinerCLI.kt
+++ src/main/kotlin/cli/gitMinersCLI/FileDependencyMatrixMinerCLI.kt
@@ -1,19 +1,12 @@
 package cli.gitMinersCLI
 
 import cli.InfoCLI
-import cli.UtilCLI
-import com.github.ajalt.clikt.parameters.options.default
-import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.types.int
 import gitMiners.FileDependencyMatrixMiner
-import util.ProjectConfig
 
-// TODO: add multithreading abstract miner class
 class FileDependencyMatrixMinerCLI :
-    GitMinerCLI(InfoCLI("FileDependencyMatrixMiner", "Mine technical dependencies based on commits")) {
-    private val numThreads by option("-n", "--num-threads", help = UtilCLI.helpNumThreads)
-        .int()
-        .default(ProjectConfig.numThreads)
+    MultithreadedGitMinerCLI(
+        InfoCLI("FileDependencyMatrixMiner", "Mine technical dependencies based on commits")
+    ) {
 
     override fun run() {
         val miner = FileDependencyMatrixMiner(repository!!, branches, numThreads = numThreads)
