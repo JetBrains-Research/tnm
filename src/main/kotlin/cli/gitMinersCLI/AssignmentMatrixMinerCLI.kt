fee0dc4e476fc3f76eb0e5871be46f60ef42c8a7
NikolaiSv <nikolai.sviridov@jetbrains.com>
2021-01-12 12:50:23 +0300

    Add cli thread option

--- src/main/kotlin/cli/gitMinersCLI/AssignmentMatrixMinerCLI.kt
+++ src/main/kotlin/cli/gitMinersCLI/AssignmentMatrixMinerCLI.kt
@@ -1,12 +1,22 @@
 package cli.gitMinersCLI
 
 import cli.InfoCLI
+import cli.UtilCLI
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.int
 import gitMiners.AssignmentMatrixMiner
+import util.ProjectConfig
 
+// TODO: add multithreading abstract miner class
 class AssignmentMatrixMinerCLI :
     GitMinerCLI(InfoCLI("AssignmentMatrixMiner", "Mine the assignments of people to a technical entities")) {
+    private val numThreads by option("-n", "--num-threads", help = UtilCLI.helpNumThreads)
+        .int()
+        .default(ProjectConfig.numThreads)
+
     override fun run() {
-        val miner = AssignmentMatrixMiner(repository!!, branches)
+        val miner = AssignmentMatrixMiner(repository!!, branches, numThreads = numThreads)
         miner.run()
         miner.saveToJson(resources!!)
     }
