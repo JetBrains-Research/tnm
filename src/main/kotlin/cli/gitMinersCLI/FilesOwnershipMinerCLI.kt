b2edf03165a0b3f540caa8881a37aea7a389b590
Nikolai Sviridov <Nikolai.Sviridov@jetbrains.com>
2021-01-15 04:20:31 +0300

    Fix FilesOwnershipMinerCLI msg

--- src/main/kotlin/cli/gitMinersCLI/FilesOwnershipMinerCLI.kt
+++ src/main/kotlin/cli/gitMinersCLI/FilesOwnershipMinerCLI.kt
@@ -7,7 +7,7 @@ import util.ProjectConfig
 class FilesOwnershipMinerCLI : GitMinerCLI(
     InfoCLI(
         "FilesOwnershipMiner",
-        "Miner yields JSON file ${ProjectConfig.FILES_OWNERSHIP} with map of maps, where the outer " +
+        "Miner yields JSON file ${ProjectConfig.DEVELOPER_KNOWLEDGE} with map of maps, where the outer " +
                 "key is the user id, the inner key is the file id and the value is the developer knowledge about the file."
     )
 ) {
