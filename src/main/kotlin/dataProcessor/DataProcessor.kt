8ce0093c488f93c88e52c8b5d0a193acc4b212c5
Nikolai Sviridov <Nikolai.Sviridov@jetbrains.com>
2021-04-19 17:14:05 +0300

    Add comment for DataProcessor.kt

--- src/main/kotlin/dataProcessor/DataProcessor.kt
+++ src/main/kotlin/dataProcessor/DataProcessor.kt
@@ -1,5 +1,10 @@
 package dataProcessor
 
+/**
+ * Interface for processing incoming data.
+ * All research techniques use different types of data. To unify such cases, this interface got generic type D,
+ * which stands for data type needed for each technique.
+ */
 
 interface DataProcessor<D> {
     fun processData(data: D)
