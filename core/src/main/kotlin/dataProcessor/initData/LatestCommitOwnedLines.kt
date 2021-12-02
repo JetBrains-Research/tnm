3c1aeab3c9b45362ade272971908fc27628c148a
Nikolai Sviridov <nikolai.sviridov@jetbrains.com>
2021-12-02 15:59:49 +0300

    Add serializer for date, make serializable FileLineOwnedByUser, LatestCommitOwnedLines

--- core/src/main/kotlin/dataProcessor/initData/LatestCommitOwnedLines.kt
+++ core/src/main/kotlin/dataProcessor/initData/LatestCommitOwnedLines.kt
@@ -1,7 +1,13 @@
 package dataProcessor.initData
 
 import dataProcessor.initData.entity.FileLineOwnedByUser
+import kotlinx.serialization.Serializable
+import util.serializers.DateAsLongSerializer
 import java.util.*
 
-data class LatestCommitOwnedLines(val latestCommitDate: Date, val linesOwnedByUser: List<FileLineOwnedByUser>) :
+@Serializable
+data class LatestCommitOwnedLines(
+    @Serializable(with = DateAsLongSerializer::class)
+    val latestCommitDate: Date,
+    val linesOwnedByUser: List<FileLineOwnedByUser>) :
     InitData
--- core/src/main/kotlin/dataProcessor/initData/entity/FileLineOwnedByUser.kt
+++ core/src/main/kotlin/dataProcessor/initData/entity/FileLineOwnedByUser.kt
@@ -1,3 +1,6 @@
 package dataProcessor.initData.entity
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class FileLineOwnedByUser(val lineNumber: Int, val filePath: String, val user: String)
