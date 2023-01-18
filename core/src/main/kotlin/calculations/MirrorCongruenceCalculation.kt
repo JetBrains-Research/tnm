42edde2d71cd4b248e070f9f70b25d4a16f9a139
Nikolai Sviridov <nikolaisvg@gmail.com>
2023-01-18 15:37:36 +0100

    Change MirrorCongruenceCalculation

--- core/src/main/kotlin/calculations/MirrorCongruenceCalculation.kt
+++ core/src/main/kotlin/calculations/MirrorCongruenceCalculation.kt
@@ -1,70 +1,48 @@
 package calculations
 
-import util.Graph
-
 object MirrorCongruenceCalculation {
 
     fun run(
-        artifactsRelations: Array<Array<Int>>,
-        assignmentMatrix: Array<Array<Int>>,
-        developersRelations: Array<Array<Int>>
+        Gs: Map<Int, Set<Int>>,
+        Gp: Map<Int, Set<Int>>,
+        J: Map<Int, Set<Int>>,
     ): Float {
-        val Gs = createGraph(artifactsRelations)
-        val Gp = createGraph(developersRelations)
-        val J = createJ(
-            artifactsRelations,
-            assignmentMatrix
-        )
 
         var y = 0
         var k = 0
 
-        val numOfArtifacts = artifactsRelations.size
-        for (i in 0 until numOfArtifacts) {
-            for (j in 0 until numOfArtifacts) {
-                if (Gs.adjacencyMap[i]?.contains(j) == true) {
-                    for (userId1 in J.adjacencyMap[i]!!) {
-                        for (userId2 in J.adjacencyMap[j]!!) {
-                            if (userId1 != userId2) y++
-                            if (Gp.adjacencyMap[userId1]?.contains(userId2) == true) k++
-                        }
+        val inverseJ = inverseJ(J)
+
+        for ((i, adjS) in Gs) {
+            for (j in adjS) {
+                // arc (i, j)
+                val Ji = inverseJ[i] ?: continue
+                val Jj = inverseJ[j] ?: continue
+
+                for (userId1 in Ji) {
+                    for (userId2 in Jj) {
+                        if (userId1 != userId2) y++
+
+                        val isFamiliar = Gp[userId1]?.contains(userId2) == true ||
+                            Gp[userId2]?.contains(userId1) == true
+                        if (isFamiliar) k++
                     }
                 }
             }
         }
-
         val congruence = k.toFloat() / y.toFloat()
         return congruence
     }
 
-    private fun createGraph(adjacencyMatrix: Array<Array<Int>>): Graph<Int> {
-        val result = Graph<Int>()
-        val numOfRows = adjacencyMatrix.size
-        val numOfColumns = adjacencyMatrix[0].size
-        for (i in 0 until numOfRows) {
-            for (j in 0 until numOfColumns) {
-                if (i == j) continue
-                if (adjacencyMatrix[i][j] != 0) {
-                    result.addEdge(i, j)
-                }
+    private fun inverseJ(J: Map<Int, Set<Int>>): Map<Int, Set<Int>> {
+        val result = HashMap<Int, MutableSet<Int>>()
+
+        for ((user, artifacts) in J) {
+            for (artifact in artifacts) {
+                result.computeIfAbsent(artifact) { mutableSetOf() }.add(user)
             }
         }
+
         return result
     }
-
-    private fun createJ(
-        artifactsRelations: Array<Array<Int>>,
-        assignmentMatrix: Array<Array<Int>>,
-    ): Graph<Int> {
-        val result = Graph<Int>()
-        val numOfArtifacts = artifactsRelations.size
-        val numOfUsers = assignmentMatrix.size
-        for (i in 0 until numOfUsers) {
-            for (j in 0 until numOfArtifacts) {
-                if (assignmentMatrix[i][j] != 0) result.addEdge(j, i)
-            }
-        }
-        return result
-    }
-
 }
